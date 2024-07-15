059a65cae2b2b49990bfaa6b3367521ff46134e6
Deeppy1 <161700797+Deeppy1@users.noreply.github.com>
2024-07-15 10:48:29 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,32 +1,30 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showLeds(`
+        . . . . .
+        . # # # .
+        . # # # .
+        . # # # .
+        . . . . .
+        `)
+    pins.digitalWritePin(DigitalPin.P0, 1)
+    basic.pause(500)
+    pins.digitalWritePin(DigitalPin.P0, 0)
+    basic.clearScreen()
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showLeds(`
+        . . . . .
+        # # # # #
+        # # # # #
+        # # # # #
+        . . . . .
+        `)
+    pins.digitalWritePin(DigitalPin.P1, 1)
+    basic.pause(2000)
+    pins.digitalWritePin(DigitalPin.P1, 0)
+    basic.clearScreen()
+})
 basic.clearScreen()
 let morse_variable = 0
 pins.digitalWritePin(DigitalPin.P0, 0)
 pins.digitalWritePin(DigitalPin.P1, 0)
-basic.forever(function () {
-    if (input.buttonIsPressed(Button.A)) {
-        basic.showLeds(`
-            . . . . .
-            . # # # .
-            . # # # .
-            . # # # .
-            . . . . .
-            `)
-        pins.digitalWritePin(DigitalPin.P0, 1)
-    } else {
-        pins.digitalWritePin(DigitalPin.P0, 0)
-        basic.clearScreen()
-    }
-    if (input.buttonIsPressed(Button.B)) {
-        basic.showLeds(`
-            . . . . .
-            # # # # #
-            # # # # #
-            # # # # #
-            . . . . .
-            `)
-        pins.digitalWritePin(DigitalPin.P1, 1)
-    } else {
-        pins.digitalWritePin(DigitalPin.P1, 0)
-        basic.clearScreen()
-    }
-})
