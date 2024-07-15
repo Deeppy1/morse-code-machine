4df20aac8462481fe0dc9493247a718dad2c437a
Deeppy1 <161700797+Deeppy1@users.noreply.github.com>
2024-07-15 10:15:45 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,5 @@
 input.onButtonPressed(Button.A, function () {
-    basic.showNumber(0)
-    pins.digitalWritePin(DigitalPin.P0, 0)
+	
 })
 input.onButtonPressed(Button.AB, function () {
 	
@@ -9,6 +8,20 @@ input.onButtonPressed(Button.B, function () {
     basic.showNumber(1)
     pins.digitalWritePin(DigitalPin.P1, 1)
 })
+let morse_variable = 0
+pins.digitalWritePin(DigitalPin.P0, 0)
+pins.digitalWritePin(DigitalPin.P1, 0)
 basic.forever(function () {
-	
+    if (input.buttonIsPressed(Button.A)) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+        pins.digitalWritePin(DigitalPin.P0, 1)
+    } else {
+        pins.digitalWritePin(DigitalPin.P0, 0)
+    }
 })
